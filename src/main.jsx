7925c00279c6f6da34fdb72150e91474ea8592c1
luv242008-dev <luv242008@gmail.com>
2026-09-22 19:18:13 +0200

    Additive NEXUS enhancements entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import NexusEnhancements from './nexusEnhancements';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <NexusEnhancements />
   </React.StrictMode>
 );
 
